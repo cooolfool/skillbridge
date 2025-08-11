932453e7642ca14047e47461b560fe470a9aa58a
cooolfool <pratyushshandilya02@gmail.com>
2025-08-11 23:05:57 +0530

    Add logging to coldStartSolver scheduled task
    Introduced SLF4J logging to the coldStartSolver class and added an info log statement to the scheduled method to indicate when the cron job runs.

--- backend/src/main/java/com/skillbridge/crons/coldStartSolver.java
+++ backend/src/main/java/com/skillbridge/crons/coldStartSolver.java
@@ -1,16 +1,19 @@
 package com.skillbridge.crons;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class coldStartSolver {
 
     @Scheduled(cron = "0 * * * * *")
     public void runEveryMinute(){
 
         //Running just to keep the server up
+        log.info("Cron running.");
 
     }
 }
