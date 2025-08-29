b1ba3cbd1a67d4beb5be4b26d005a5a7943a769f
cooolfool <pratyushshandilya02@gmail.com>
2025-08-29 18:27:24 +0530

    Update LikeProjectController.java

--- backend/src/main/java/com/skillbridge/controller/LikeProjectController.java
+++ backend/src/main/java/com/skillbridge/controller/LikeProjectController.java
@@ -4,6 +4,7 @@ import com.skillbridge.entity.UserEntity;
 import com.skillbridge.service.LikeProjectService;
 import com.skillbridge.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/projects")
 @RequiredArgsConstructor
+@Slf4j
 public class LikeProjectController {
 
     private final LikeProjectService likeProjectService;
@@ -19,7 +21,9 @@ public class LikeProjectController {
 
     @PostMapping("/{projectId}/like")
     public ResponseEntity<String> likeProject(@PathVariable Long projectId,
-                                              @RequestHeader("authToken") String token) {
+                                              @RequestHeader(value = "authToken") String token) {
+        log.info("Request in like project controller");
+        log.info("Request Hearders : {}",token);
         UserEntity loggedInUser = userService.loggedInUser(token);
         likeProjectService.likeProject(projectId, loggedInUser.getId());
         return ResponseEntity.ok("Project liked successfully");
@@ -27,7 +31,9 @@ public class LikeProjectController {
 
     @PostMapping("/{projectId}/unlike")
     public ResponseEntity<Map<String,String>> unlikeProject(@PathVariable Long projectId,
-                                                @RequestHeader("authToken") String token) {
+                                                            @RequestHeader(value = "authToken") String token) {
+        log.info("Request in unlike project controller");
+        log.info("Request Hearders : {}",token);
         UserEntity loggedInUser = userService.loggedInUser(token);
         likeProjectService.unlikeProject(projectId, loggedInUser.getId());
         return ResponseEntity.ok(Map.of("message", "Project liked successfully"));
@@ -35,12 +41,15 @@ public class LikeProjectController {
 
     @GetMapping("/{projectId}/likes/count")
     public ResponseEntity<Long> getLikeCount(@PathVariable Long projectId) {
+        log.info("Request in get likes count");
         return ResponseEntity.ok(likeProjectService.getLikeCount(projectId));
     }
 
     @GetMapping("/{projectId}/likes/status")
     public ResponseEntity<Boolean> hasUserLiked(@PathVariable Long projectId,
                                                 @RequestHeader("authToken") String token) {
+        log.info("Request in get liked status");
+        log.info("Request Hearders : {}",token);
         UserEntity loggedInUser = userService.loggedInUser(token);
         return ResponseEntity.ok(likeProjectService.hasUserLiked(projectId, loggedInUser.getId()));
     }
